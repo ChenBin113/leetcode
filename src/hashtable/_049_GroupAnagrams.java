430ccaa6df71cddbeef15831c6fbebb09f946740
ChenBin113 <m13680309305_1@163.com>
2019-10-17 11:15:17 +0800

    Add HashTable part.

---
+++ src/hashtable/_049_GroupAnagrams.java
@@ -0,0 +1,27 @@
+package hashtable;
+
+import java.util.*;
+
+public class _049_GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            //排序工作：String -> char[] -> String
+            char[] ca = str.toCharArray();
+            Arrays.sort(ca);
+            //String s = ca.toString(); //这个是不通过的，原因是，toString() 方法的误用
+            String s = String.valueOf(ca);//这个方法才是转成 String 字符串
+
+            //检查 s 是否在 key 中，不是则添加
+            if (!map.containsKey(s)) map.put(s, new ArrayList<>());
+            map.get(s).add(str);
+        }
+        return new ArrayList(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        groupAnagrams(strs);
+    }
+}
---
+++ src/hashtable/_409_LongestPalindrome.java
@@ -0,0 +1,22 @@
+package hashtable;
+
+public class _409_LongestPalindrome {
+    public int longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int maxLength = 0;
+        int flag = 0;
+        char[] map = new char[128];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+        }
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] % 2 == 0) {
+                maxLength += map[i];
+            } else {
+                maxLength += map[i] - 1;
+                flag = 1;
+            }
+        }
+        return maxLength + flag;
+    }
+}
